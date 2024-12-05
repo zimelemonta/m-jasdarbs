3156d97ece72bba43f00dc545ea5f24d97cf7288
zimelemonta <ziemelemonta@gmail.com>
2024-12-05 18:51:57 +0200

    Add files via upload

---
+++ 11_nedēļa1.py
@@ -0,0 +1,52 @@
+#saraksts1 = [1, 2, 3, 4, 5, 6, 7, 8, 9] #izveidojam jaunu sarakstu ar 5 elementiem
+
+#print(saraksts1[4]) #lai izvadītu 5 ir jāraksta 4, ja 4 tad 3 un, ja 3 tad 2 un tātālāk
+
+#for i in range(0, len(saraksts1)): #in rage nosaka  for i in range [start, end]
+   # print("i; ",i)
+   # print(saraksts1[i])
+    
+#print()  
+#for i in range( len(saraksts1)-1, -1, -1):
+   #  print("i=", i)
+    # print(saraksts1[i])
+"""
+garums = int(input('augstums: '))
+augstums = int(input('garums: '))
+
+
+for i in range(0,garums):
+ #  for j in range(0,augstums):
+        print('#', end= " ")
+    print()
+    
+print()
+     
+for i in range(0,garums):
+    for j in range(0,garums):
+         print('#', end= " ")
+    print()
+"""
+
+
+augstums = 5
+
+for i in range(0, augstums):
+    for j in range(0, augstums*2):
+        if i == j:
+            
+
+    for i in range(0, augstums + 0)
+        
+        spaces = ' ' * (augstums - i)
+       
+        stars = '' * (0 * i - 2)
+        print(spaces + stars)
+
+t
+    height = int(input("10 "))
+    if height > 0:
+        
+    else:
+        print()
+
